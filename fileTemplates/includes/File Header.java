1846b6bbabd8f13ce4a1d9d67f88bd7a077612ad
hzy24985 <hzy24985@ly.com>
2022-03-18 16:26:46 +0800

    IU-2021.1.1 <alex@Alexhu.local Create markdown.xml, IntelliLang.xml, project.default.xml, equalsHashCodeTemplates.xml, Enum.java, Interface.java, coverVersions.xml, Default.xml, ignore.xml, find.xml, File Header.java, YapixShowdocSettings.xml, code.style.schemes.xml, testme.file.template.settings.xml, KeyPromoterXSettings.xml, _@user_Darcula.icls, path.macros.xml, Interface JavaDoc.tpl, javarebel.xml, Set Method JavaDoc.tpl, cosy_setting.xml, Default.xml, _@user_Material Darker.icls, CoverTelemetryId.xml, YapixSettings.xml, Class JavaDoc.tpl, Method JavaDoc.tpl, lightEdit.xml, alibaba-toolkit-startUp.xml, CoverSettingsConfig.xml, YapixYapiSettings.xml, Throws clause JavaDoc.tpl, keymap.xml, YapixRap2Settings.xml, Return type JavaDoc.tpl, editor.xml, applicationLibraries.xml, debugger.xml, updates.xml, baseRefactoring.xml, ide.general.xml, other.xml, web-types-registry.xml, ide-features-trainer.xml, eclipseCodeFormatter.xml, Constructor JavaDoc.tpl, CodeGlance.xml, rainbow_brackets.xml, live-plugin.xml, lombok-plugin.xml, git.xml, profiler.xml, shared-indexes.xml, Parameter JavaDoc.tpl, ActionScript File Header.as, generic-key-value-store.xml, ui.lnf.xml, Get Method JavaDoc.tpl, filetypes.xml, projectView.xml, lermitage-extra-icons.xml, plugin_ui.xml, watcherDefaultTasks.xml, colors.scheme.xml, External Tools.xml, notifications.xml, archetypeCatalogs.xml, databaseDrivers.xml, templates.xml, stringManipulation.xml, JAVA_HEADER.java, alibaba-toolkit-edas.xml, MyLy.xml, Class.java, jdk.table.xml, IntelliJ IDEA Classic copy.xml, mavenRunHelper.xml, acceptedLanguageLevels.xml, laf.xml, macros.xml, proxy.settings.xml, vcs.xml, databaseSettings.xml, grazie_global.xml, toStringTemplates.xml, YapixEolinkerSettings.xml, usageView.xml, setterTemplates.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,5 @@
+/**
+ * 
+ * @author hzy24985
+ * @version $Id: ${NAME}, v 0.1 ${DATE} ${TIME} hzy24985 Exp $
+ */
---
+++ fileTemplates/internal/Enum.java
@@ -0,0 +1,4 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("JAVA_HEADER.java")
+public enum ${NAME} {
+}
